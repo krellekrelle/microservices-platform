7066048cf82bcd26d5d0a23125d2eede2fe10b52
kristian <klarsen1997@gmail.com>
2025-08-04 02:12:53 +0200

    Add favicon to all services and HTML pages
    - Created professional microservices-themed SVG favicon
    - Added favicon.svg to all service public directories
    - Updated all HTML files to include favicon link tags
    - Updated dynamically generated login HTML to include favicon
    - Favicon shows connected dots representing microservices architecture

--- landing-page/server.js
+++ landing-page/server.js
@@ -64,6 +64,8 @@ async function initializeApp() {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Personal Project - Login</title>
+    <link rel="icon" type="image/svg+xml" href="/static/favicon.svg">
+    <link rel="alternate icon" href="/static/favicon.svg">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
